2b7430acddcf1ebf8f9f9ca16797375189c150c5
edwinwow <yunlong.he@hotmail.com>
2014-03-24 11:58:17 +0800

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,38 @@ class MissConnection(Displayable, Ownable):
   @models.permalink
     def get_absolute_url(self):
         return ("miss_connection_detail", (), {"slug": self.slug})
-        
+
+
+class GalleryImage(Orderable):
+
+    missconnection = models.ForeignKey("MissConnection", related_name="images")
+    file = FileField(_("File"), max_length=200, format="Image",
+        upload_to=upload_to("galleries.GalleryImage.file", "galleries"))
+
+    class Meta:
+        verbose_name = _("Image")
+        verbose_name_plural = _("Images")
+
+    def __str__(self):
+        return self.description
+
+    def save(self, *args, **kwargs):
+        """
+If no description is given when created, create one from the
+file name.
+"""
+        if not self.id and not self.description:
+            name = force_text(self.file.name)
+            name = name.rsplit("/", 1)[-1].rsplit(".", 1)[0]
+            name = name.replace("'", "")
+            name = "".join([c if c not in punctuation else " " for c in name])
+            # str.title() doesn't deal with unicode very well.
+            # http://bugs.python.org/issue6412
+            name = "".join([s.upper() if i == 0 or name[i - 1] == " " else s
+                            for i, s in enumerate(name)])
+            self.description = name
+        super(GalleryImage, self).save(*args, **kwargs)
+
         
 class Profile(models.Model):
 
